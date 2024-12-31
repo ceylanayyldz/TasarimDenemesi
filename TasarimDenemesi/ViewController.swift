d73026197c174c115b52ec2d4648b4ae335de5c0
Ceylan Ayyildiz Yildirim <ceylan95.ca@gmail.com>
2024-12-31 20:47:19 +0300

    First Commit

---
+++ TasarimDenemesi/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TasarimDenemesi
+//
+//  Created by Ceylan Ayyildiz on 17.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!,.font: UIFont(name: "Pacifico-Regular", size: 22)! ]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        
+        navigationController?.navigationBar.compactAppearance = appearance
+        
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+        
+        
+        
+        
+    }
+
+
+}
+
